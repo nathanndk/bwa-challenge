ead96e9bcc9ddd1afe9e1fad200e2d97dfa94c0e
nathanndk <Razor091203@gmail.com>
2024-04-18 11:22:32 +0700

    fix migration

--- database/migrations/0003_03_03_000000_create_categories_table.php
+++ database/migrations/0003_03_03_000000_create_categories_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('name');
             $table->string('slug')->unique();
             $table->string('icon')->nullable();
-            $table->foreignId('teacher_id')->constrained('teachers')->onDelete('cascade');
             $table->timestamps();
         });
 
